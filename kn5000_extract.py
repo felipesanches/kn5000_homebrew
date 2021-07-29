190c785e65c51048c4a75938fd58ed7c43f5fafc
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2021-07-29 02:16:28 -0300

    also use the lzss module on kn500_extract.py

--- kn5000_extract.py
+++ kn5000_extract.py
@@ -4,52 +4,19 @@
 # This program loads a compressed firmware update file and
 # outputs its raw, uncompressed contents.
 
-compressed = open("HKMSPRG.SLD.v10", "rb").read()
-raw = open("HKMSPRG.SLD.v10.raw", "wb")
+import lzss
 
-data_buffer = [0x00] * 4096
-read_pointer = 0x0B # skip header bytes: "SLIDE4K", 00 20 00 00
-write_pointer = 0x00
+compressed_file = open("HKMSPRG.SLD.v10", "rb")
+raw_file = open("HKMSPRG.SLD.v10.raw", "wb")
 
-def output(data_byte):
-  global write_pointer
-  global data_buffer
-  data_buffer[write_pointer%4096] = data_byte
-  raw.write(bytes([data_byte]))
-  write_pointer += 1
+# skip the header:
+expected_header = b"SLIDE4K\x00\x20\x00\x00"
+header = compressed_file.read(len(expected_header))
+assert header == expected_header
 
+# and read the rest
+compressed_data = compressed_file.read()
 
-def fetch_byte():
-  global read_pointer
-  data = compressed[read_pointer]
-  read_pointer += 1
-  return data
-
-
-while read_pointer < len(compressed):
-  counter = 0
-  compression_map_byte = fetch_byte()
-#  print(hex(compression_map_byte))
-  while counter < 8:
-    is_raw = compression_map_byte & 1
-    compression_map_byte >>= 1
-    counter += 1
-
-    if is_raw:
-      output(fetch_byte())
-    else:
-      try:
-        compression_word = fetch_byte()
-        compression_word |= fetch_byte() << 8
-      except:
-        import sys
-        sys.exit(f"DEBUG: Final context: compression_map_byte={compression_map_byte} counter={counter} read_pointer={read_pointer} len(compressed)={len(compressed)}")
-        
-      chunk_length = 3 + ((compression_word & 0x0F00) >> 8)
-      chunk_address = ((compression_word & 0xF000) >> 4) | (compression_word & 0xFF)
-#      print(f'chunk_length: {hex(chunk_length)}\tchunk_address: {hex(chunk_address)}')
-      while chunk_length > 0:
-        output(data_buffer[((chunk_address+18)%4096)])
-        chunk_address += 1
-        chunk_length -= 1
+# decompress the data and save it
+raw_file.write(lzss.decompress(data=compressed_data, initial_buffer_value=b'\x00'))
 
