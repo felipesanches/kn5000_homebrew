b35398e7001e2bd184231aed0959ebaf26e88635
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-01-22 23:56:50 -0300

    wip

--- demos/anotherworld/another.asm
+++ demos/anotherworld/another.asm
@@ -23,42 +23,20 @@ ENTRY:
 	LD XWA, 0
 
 MAIN_LOOP:
-	LD XHL, BITMAP_2
+	LD XHL, BITMAP_1
 	CALL DRAW_BITMAP
 
 	LD WA, 7
 	CALL SETUP_PALETTE
 	CALL PAUSE
 
-	LD WA, 8
-	CALL SETUP_PALETTE
-	CALL PAUSE
-
-	LD WA, 9
-	CALL SETUP_PALETTE
-	CALL PAUSE
-
-	LD WA, 10
-	CALL SETUP_PALETTE
-	CALL PAUSE
-	
-	LD XHL, BITMAP_1
-	CALL DRAW_BITMAP
-
-	LD WA, 11
-	CALL SETUP_PALETTE
-	CALL PAUSE
-
-	LD WA, 12
-	CALL SETUP_PALETTE
-	CALL PAUSE
-
-	LD WA, 13
-	CALL SETUP_PALETTE
-	CALL PAUSE
-
-	LD WA, 14
-	CALL SETUP_PALETTE
+;	(CINEMATIC_CARKEY, COLOR_BLACK=0xFF, zoom=0x40, x=160, y=100);
+	LD XIX, INTRO_VIDEO_1
+	ADD XIX, 0F6D2h			; CINEMATIC_CARKEY
+	LD DE, 160
+	LD HL, 100
+	LD BC, 0FF40h
+	CALL readAndDrawPolygon
 	CALL PAUSE
 
 	JP MAIN_LOOP
@@ -70,6 +48,110 @@ PAUSE_LOOP1:
 PAUSE_LOOP2:
 	DJNZ DE, PAUSE_LOOP2
 	DJNZ BC, PAUSE_LOOP1
+	RET
+
+readAndDrawPolygon:
+	; Inputs:
+	; XIX: polygon data pointer
+	; DE: x
+	; HL: y
+	; B: color (Black = FFh)
+	; C: zoom (default = 40h)
+	PUSH DE
+	PUSH HL
+	PUSH BC
+	
+	PUSH XIX
+	LD A, (XIX)
+	INC XIX
+	CP A, 0C0H
+	JP ULT, VALUE_IS_LT_C0
+
+VALUE_IS_GTE_C0:
+	POP XIX
+	;(&m_polygonData[m_data_offset], zoom, m_polygonData, 0);
+	call readVertices
+	; (color, pt)
+	CALL fillPolygon
+	JP end_of_readAndDrawPolygon
+
+VALUE_IS_LT_C0: ; for now, here we simply assume value is == 2 without checking.
+	;(zoom, pt)
+	PUSH BC ; C=zoom
+	PUSH DE ; x
+	PUSH HL ; y
+	CALL readAndDrawPolygonHierarchy
+	POP BC
+	POP DE
+	POP HL
+
+	POP XIX
+
+end_of_readAndDrawPolygon:
+	POP BC
+	POP HL
+	POP DE
+	RET
+
+readVertices:
+	; implement-me!
+	RET
+
+fillPolygon:
+	; implement-me!
+	RET
+
+readAndDrawPolygonHierarchy:
+
+	;	pt.x -= m_polygonData[m_data_offset++] * zoom / DEFAULT_ZOOM;
+	;	pt.y -= m_polygonData[m_data_offset++] * zoom / DEFAULT_ZOOM;
+	LD WA, 0
+	LD A, (XIX)
+	INC XIX
+	LD HL, (XSP + 6)
+	MUL XHL, WA			; PT.X *= ZOOM
+	SRAW 6, HL			; PT.X /= default_zoom (40h)
+	LD (XSP + 6), HL
+	
+	LD WA, 0
+	LD A, (XIX)
+	INC XIX
+	LD HL, (XSP + 4)
+	MUL XHL, WA				; PT.Y *= ZOOM
+	SRAW 6, HL				; PT.Y /= default_zoom (40h)
+	LD (XSP + 4), HL
+
+	ld B, 0
+	LD C, (XIX)
+	INC XIX
+	
+children_loop:
+	PUSH HL
+	; HL = offset
+	LD HL, (XIX)
+	POP HL
+
+	; VMPoint po(pt);
+	; po.x += m_polygonData[m_data_offset++] * zoom / DEFAULT_ZOOM;
+	; po.y += m_polygonData[m_data_offset++] * zoom / DEFAULT_ZOOM;
+
+	; uint16_t color = 0xFF;
+	; if (offset & 0x8000)
+	; {
+	; 	color = m_polygonData[m_data_offset++] & 0x7F;
+	; 	m_data_offset++; //and waste a byte...
+	; }
+
+	; uint16_t backup = m_data_offset;
+
+	; m_data_offset = (offset & 0x7FFF) * 2;
+
+	; (color, zoom, po)
+	CALL readAndDrawPolygon
+
+	; m_data_offset = backup;
+	
+	DJNZ BC, children_loop
 	RET
 
 DRAW_BITMAP:
