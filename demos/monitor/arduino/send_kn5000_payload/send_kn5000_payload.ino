9b4b02e3ad7e8eb41875eb6dc317525b78498225
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-10-28 20:29:14 -0300

    wip Arduino - PLEASE REVIEW THIS!!!!

--- demos/monitor/arduino/send_kn5000_payload/send_kn5000_payload.ino
+++ demos/monitor/arduino/send_kn5000_payload/send_kn5000_payload.ino
@@ -6,21 +6,18 @@ const byte pA4 = 6;
 const byte pA5 = 7;
 const byte pA6 = 8;
 const byte pA7 = 9;
-const byte pC0 = A0;
-const byte pC1 = A1;
-const byte pC2 = A2;
-const byte pB3 = A3;
-const byte pB4 = A4;
-const byte pB5 = A5;
-
+const byte pC0_KN_Strobe = A0;
+const byte pC1_KN_Speaking = A1;
+const byte pC2_KN_OK = A2;
+const byte pB3_PC_Strobe = A3;
+const byte pB4_PC_Speaking = A4;
+const byte pB5_PC_OK = A5;
 byte pA = 0;
-byte pBC = 0;
+byte pB = 0;
+byte pC = 0;
+byte state = 0;
 
-void setup() {
-//  Serial.begin(230400);
-  Serial.begin(115200);
-  //Serial.write("HD-AE5000 READ:\n");
-  
+void set_port_A_INPUT(){
   pinMode(pA0, INPUT);
   pinMode(pA1, INPUT);
   pinMode(pA2, INPUT);
@@ -29,53 +26,94 @@ void setup() {
   pinMode(pA5, INPUT);
   pinMode(pA6, INPUT);
   pinMode(pA7, INPUT);
-  pinMode(pC0, INPUT);
-  pinMode(pC1, OUTPUT);
-  pinMode(pC2, INPUT);
-  pinMode(pB3, INPUT);
-  pinMode(pB4, INPUT);
-  pinMode(pB5, INPUT);
 }
 
-void print_hex(byte v){
-  byte d = (v >> 4) & 0xF;
-  Serial.write(d < 10 ? d + '0': d - 10 + 'A');
-  d = v & 0xF;
-  Serial.write(d < 10 ? d + '0': d - 10 + 'A');
+void set_port_A_OUTPUT(){
+  pinMode(pA0, OUTPUT);
+  pinMode(pA1, OUTPUT);
+  pinMode(pA2, OUTPUT);
+  pinMode(pA3, OUTPUT);
+  pinMode(pA4, OUTPUT);
+  pinMode(pA5, OUTPUT);
+  pinMode(pA6, OUTPUT);
+  pinMode(pA7, OUTPUT);
+}
+
+byte read_A(){
+	byte v=0;
+	v = v << 1 | (digitalRead(pA7) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pA6) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pA5) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pA4) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pA3) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pA2) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pA1) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pA0) == HIGH ? 1: 0);
+	return v;
+}
+
+void write_A(byte v){
+	digitalWrite(pA7, v & 0x80 ? HIGH : LOW);
+	digitalWrite(pA6, v & 0x40 ? HIGH : LOW);
+	digitalWrite(pA5, v & 0x20 ? HIGH : LOW);
+	digitalWrite(pA4, v & 0x10 ? HIGH : LOW);
+	digitalWrite(pA3, v & 0x08 ? HIGH : LOW);
+	digitalWrite(pA2, v & 0x04 ? HIGH : LOW);
+	digitalWrite(pA1, v & 0x02 ? HIGH : LOW);
+	digitalWrite(pA0, v & 0x01 ? HIGH : LOW);
+}
+
+void write_B(byte v){
+	digitalWrite(pB5_PC_OK, v & 4 ? HIGH : LOW);
+	digitalWrite(pB4_PC_Speaking, v & 2 ? HIGH : LOW);
+	digitalWrite(pB3_PC_Strobe, v & 1 ? HIGH : LOW);
+}
+
+byte read_C(){
+	byte v=0;
+	v = v << 1 | (digitalRead(pC2_KN_OK) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pC1_KN_Speaking) == HIGH ? 1: 0);
+	v = v << 1 | (digitalRead(pC0_KN_Strobe) == HIGH ? 1: 0);
+	return v;
+}
+
+enum {
+	WAITING_KN_ID = 0,
+	
+}
+
+void setup() {
+	Serial.begin(115200);
+	//Serial.write("HD-AE5000 READ:\n");
+	set_port_A_INPUT()
+	pinMode(pC0_KN_Strobe, INPUT);
+	pinMode(pC1_KN_Speaking, INPUT);
+	pinMode(pC2_KN_OK, INPUT);
+	pinMode(pB3_PC_Strobe, OUTPUT);
+	pinMode(pB4_PC_Speaking, OUTPUT);
+	pinMode(pB5_PC_OK, OUTPUT);
+
+	state = WAITING_KN_ID;
+}
+
+void send_byte(byte v){
+	while (KN_is_speaking()) {
+		// wait	
+	}
+	digitalWrite(pB4_PC_Speaking, HIGH);
+	digitalWrite(pB3_PC_Strobe, LOW);
+	write_A(v);
+	delay(1);
+	digitalWrite(pB3_PC_Strobe, HIGH);	
 }
 
 void loop() {
-  byte value_A = 0;
-  byte value_BC = 0;
-  value_A = value_A << 1 | (digitalRead(pA7) == HIGH ? 1: 0);
-  value_A = value_A << 1 | (digitalRead(pA6) == HIGH ? 1: 0);
-  value_A = value_A << 1 | (digitalRead(pA5) == HIGH ? 1: 0);
-  value_A = value_A << 1 | (digitalRead(pA4) == HIGH ? 1: 0);
-  value_A = value_A << 1 | (digitalRead(pA3) == HIGH ? 1: 0);
-  value_A = value_A << 1 | (digitalRead(pA2) == HIGH ? 1: 0);
-  value_A = value_A << 1 | (digitalRead(pA1) == HIGH ? 1: 0);
-  value_A = value_A << 1 | (digitalRead(pA0) == HIGH ? 1: 0);
+	byte value_A = read_A();
+	byte value_C = read_C();
 
-//  value_BC = value_BC << 1 | (digitalRead(pB5) == HIGH ? 1: 0);
-//  value_BC = value_BC << 1 | (digitalRead(pB4) == HIGH ? 1: 0);
-//  value_BC = value_BC << 1 | (digitalRead(pB3) == HIGH ? 1: 0);
-//  value_BC = value_BC << 1 | (digitalRead(pC2) == HIGH ? 1: 0);
-//  value_BC = value_BC << 1 | (digitalRead(pC1) == HIGH ? 1: 0);
-//  value_BC = value_BC << 1 | (digitalRead(pC0) == HIGH ? 1: 0);
-
-  value_BC = (digitalRead(pC0) == HIGH ? 1: 0);
-  if ((value_BC % 2) != (pBC % 2)){
-    Serial.write(value_A);
-    // print_hex(value_A);
-    // Serial.write(' ');
-    // print_hex(value_BC);
-    // if (value_A >= 'a' && value_A <='z' || value_A >= 'A' && value_A <='Z') {
-    //  Serial.write(" \"");
-    //  Serial.write(value_A);
-    //  Serial.write('"');
-    //}
-    //Serial.write('\n');
-  }
-  pA = value_A;
-  pBC = value_BC;
+	if ((value_C % 2) != (pC % 2)){
+	    Serial.write(value_A);
+	}
+	pA = value_A;
+	pC = value_C;
 }
