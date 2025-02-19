52b64934c00bbf103c8abe7767a0917966eeb4df
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2025-02-18 23:48:01 -0300

    fix asm syntax

--- demos/dump_subcpu/subcpu_receive.asm
+++ demos/dump_subcpu/subcpu_receive.asm
@@ -1,4 +1,10 @@
-	org 0x1f736
+	cpu	96c141	; Actual CPU is 94c241f
+	page	0
+	maxmode	on
+
+SC1BUF: EQU 0D4h
+
+	ORG 01F736h
 
 INTRX1_HANDLER:
 	nop
@@ -9,16 +15,33 @@ INTRX1_HANDLER:
 	push xbc
 	push xwa
 	LD E, (SC1BUF)
-    LD XBC, 0x1f7ae	; dump_address
-    LD XHL, 0x1f7b5 ; MSG_INDEX
+    LD XBC, DUMP_ADDRESS	; dump_address
+    LD XHL, 01f7b5h ; MSG_INDEX
     LD A, (XHL)
     EXTZ WA
     LD (XBC + WA), E
     INC 1, A
     CP A, 7
-    JP NZ, END ; 0x01f760
+    JP NZ, FOO ; 0x01f760
     LD A, 0
-END:
+FOO:
 	LD (XHL), A ; MSG_INDEX
-	JP T 1F7A9 ; Since this new routine is too big
+	
+	; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	JP 01F7A9h ; Since this new routine is too big
 	           ; we reuse the end of INTTX_HANDLER here.
+	; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+; !!!!
+; TODO: THIS IS WRONG!!!!!! These vars must be shared between the two patches !!!!!!!
+; !!!!
+;
+DUMP_ADDRESS: dd 0120E3h  ; we'll validate correctness of this method
+                          ; by ensuring that we receive the string "KN5000 SOUND RAM"
+                          ; which we know is located in this address of the
+                          ; subcpu executable payload in RAM
+NUM_BYTES: dw 16 ; This is the length of the known string.
+DUMP_IT: db 1    ; dumping is triggered by any non-zero value here
+MSG_INDEX: db 0  ; this variable is used for keeping track of the request message
+                 ; bytes as they arrive to the subcpu via the serial port #1
+                 ; ("COMPUTER INTERFACE" connection via a mini-din cable)
--- demos/dump_subcpu/subcpu_send.asm
+++ demos/dump_subcpu/subcpu_send.asm
@@ -1,32 +1,42 @@
-	org 0x1f765
+	cpu	96c141	; Actual CPU is 94c241f
+	page	0
+	maxmode	on
+
+SC1BUF: EQU 0D4h
+
+	ORG 01F765h
+
 INTTX1_HANDLER:
 	nop
 	nop
 	nop
-	push xhl
+	PUSH XHL
 	push xde
 	push xbc
 	push xwa
-	CP (var_1038), 0x03
-	JP NZ LABEL_1F7A9
-	LD XBC, 0x1F7AE	; dump_address
+	CP (01038h), 3     ; variable 0x1038 from the original subcpu payload
+	JP NZ, end_of_inttx1_handler
+	LD XBC, 01F7AEh	; dump_address
 	LD XDE, (XBC)
 	INC 4, XBC ; now ptr to num_bytes
 	LD HL, (XBC) ; num_bytes
 	LD A, (XBC+1)
 	CP A, 0
-	JP Z, LABEL_1F7A9
+	JP Z, end_of_inttx1_handler
     CP HL, 0
     JP NZ, not_finished_yet
     LD A, 0 ; indicate that dumping should stop
 
 not_finished_yet:
-	JP Z, LABEL_1F7A9
+	JP Z, end_of_inttx1_handler
     DEC 1, HL
     LD A, (XDE + HL) ;; dump a byte
     LD (SC1BUF), A 	; SC1BUF=D4
     LD (XBC), HL  ; num_bytes--;
-    NOP NOP
+    NOP
+    NOP
+
+end_of_inttx1_handler:
     POP XWA
     POP XBC
     POP XDE
@@ -34,8 +44,14 @@ not_finished_yet:
     RETI
 
 ;	1f7ab -> was RING_BUFFER_HAS_OVERRUN but its is OK to use
-DUMP_ADDRESS: dd
-NUM_BYTES: dw
-DUMP_IT: db 0x1
-MSG_INDEX: db 0
+
+DUMP_ADDRESS: dd 0120E3h  ; we'll validate correctness of this method
+                          ; by ensuring that we receive the string "KN5000 SOUND RAM"
+                          ; which we know is located in this address of the
+                          ; subcpu executable payload in RAM
+NUM_BYTES: dw 16 ; This is the length of the known string.
+DUMP_IT: db 1    ; dumping is triggered by any non-zero value here
+MSG_INDEX: db 0  ; this variable is used for keeping track of the request message
+                 ; bytes as they arrive to the subcpu via the serial port #1
+                 ; ("COMPUTER INTERFACE" connection via a mini-din cable)
 
